61c9eac392c1db906908f2bd627d9b712ffe85e2
Amiyaaa24 <dthaihoang014@gmail.com>
2026-06-21 10:06:16 +0700

    feat: intent tính năng share cơ bản của ứng dụng

--- app/src/main/java/com/project/smartcatch/MainActivity.kt
+++ app/src/main/java/com/project/smartcatch/MainActivity.kt
@@ -1,47 +1,51 @@
-package com.project.smartcatch
+package com.project.smartcatch // Đảm bảo đúng package name của bạn
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.project.smartcatch.ui.theme.SmartCatchTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // 1. Kiểm tra xem app được mở bằng cách bình thường hay được Share tới
+        handleSharedIntent(intent)
+
         setContent {
-            SmartCatchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            // Tạm thời hiển thị text cơ bản để test
+            Text(text = "SmartCatch đang chạy!")
+        }
+    }
+
+    // Hàm xử lý dữ liệu được share tới
+    private fun handleSharedIntent(intent: Intent?) {
+        if (intent == null) return
+
+        val action = intent.action
+        val type = intent.type
+
+        when {
+            // Trường hợp 1: Nhận 1 ảnh hoặc 1 đoạn text (link)
+            Intent.ACTION_SEND == action && type != null -> {
+                if (type.startsWith("image/")) {
+                    Log.d("SmartCatch", "Đã nhận 1 bức ảnh!")
+                    // TODO: Xử lý hiển thị URI ảnh lên Bottom Sheet
+                } else if ("text/plain" == type) {
+                    Log.d("SmartCatch", "Đã nhận 1 đoạn text/link!")
+                    // TODO: Xử lý bóc tách link
+                }
+            }
+
+            // Trường hợp 2: Nhận nhiều ảnh cùng lúc
+            Intent.ACTION_SEND_MULTIPLE == action && type != null -> {
+                if (type.startsWith("image/")) {
+                    Log.d("SmartCatch", "Đã nhận nhiều bức ảnh!")
+                    // TODO: Xử lý danh sách các URI ảnh
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCatchTheme {
-        Greeting("Android")
     }
 }
